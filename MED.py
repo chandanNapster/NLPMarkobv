133153454f03bb19085a8a2aeb6cd8cc4c3711ac
Chandan Sharma <chandan202.alive@gmail.com>
2022-10-15 16:04:18 +0530

    Changes Done

--- MED.py
+++ MED.py
@@ -24,23 +24,24 @@ my_matrix = np.zeros((row, col))
 rw = dict_word_1['p']
 co = dict_word_2[' ']
 
-def insert(word, original_letter ,replace_letter, dict):
-    word = list(word)
-    index = dict[original_letter]
-    dict[replace_letter] = dict[original_letter]
-    del dict[original_letter]
-    if index < len(word): word[index] = replace_letter
-    return ''.join(word)
 
-def delete(word, original_letter , dict):
-    word = list(word)
-    index = dict[original_letter]
-    if index < len(word) and original_letter in word: word[index] = " "
-    del dict[original_letter]
-    return ''.join(word)
+# def insert(word, original_letter ,replace_letter, dict):
+#     word = list(word)
+#     index = dict[original_letter]
+#     dict[replace_letter] = dict[original_letter]
+#     del dict[original_letter]
+#     if index < len(word): word[index] = replace_letter
+#     return ''.join(word)
+
+# def delete(word, original_letter , dict):
+#     word = list(word)
+#     index = dict[original_letter]
+#     if index < len(word) and original_letter in word: word[index] = " "
+#     del dict[original_letter]
+#     return ''.join(word)
     
-def replace(word, original_letter, replace_letter, dict):
-    return ''.join(insert(delete(word, original_letter, dict), original_letter, replace_letter, dict))
+# def replace(word, original_letter, replace_letter, dict):
+#     return ''.join(insert(delete(word, original_letter, dict), original_letter, replace_letter, dict))
     
 # print(word_1)
 
@@ -53,9 +54,9 @@ def replace(word, original_letter, replace_letter, dict):
 # print(word_1)
 
 
-word_1 = replace(word_1, 'y', 'z', dict_word_1)
+# word_1 = replace(word_1, 'y', 'z', dict_word_1)
 
-print(word_1)
+# print(word_1)
 
 
 
@@ -63,3 +64,28 @@ print(word_1)
 # del dict_word_1['p']
 
 # print(dict_word_1)
+
+
+# def insert(word, letter,index, dict):
+#     word = list(word)
+#     if word[index] == " ": word[index] = letter
+#     return ''.join(word)
+
+
+# print(insert(word_1, 'a', dict_word_1))    
+
+
+print(word_1)
+# print(dict_word_1)
+
+def delete(word, letter, dict):
+    word = list(word)
+    index = dict[letter]
+    if letter in word and index < len(word): 
+        word[index] = " "
+        del dict[letter]
+
+    return ''.join(word)     
+
+
+print(delete(delete(word_1, 'a', dict_word_1), 'p', dict_word_1))
---
+++ MinEditDistanceV1.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+class MED:
+
+    def __init__(self):
+        self.__SOURCE_WORD = " "+"play"
+        self.__TARGET_WORD = " "+ "stay"
+        self.__src_dict = dict([ (data, index) for index, data in enumerate(self.__SOURCE_WORD)])
+        self.__trg_dict = dict([ (data, index) for index, data in enumerate(self.__TARGET_WORD)])
+        self.__my_mat = np.zeros((len(self.__SOURCE_WORD), len(self.__TARGET_WORD)))
+
+    def get_source_dict(self):
+        return self.__src_dict
+
+    def set_source_word(self, word):
+        self.__SOURCE_WORD = word 
+
+    def set_target_word(self, word):
+        self.__TARGET_WORD = word       
+
+    def get_target_dict(self):
+        return self.__trg_dict
+
+    def get_src_word(self):
+        return self.__SOURCE_WORD
+
+    def get_trg_wprd(self):
+        return self.__TARGET_WORD            
+
+    def delete(self, letter, first_delete):
+        word = list(self.get_src_word())
+        dict = self.get_source_dict()
+        index = dict[letter]
+        if letter in word and first_delete:
+            word[index] = "#"
+            dict['#'] = dict[letter]
+            del dict[letter]
+        elif letter in word and not first_delete:
+            print(dict)
+            print(dict[letter])
+            print(word)
+            word[dict[letter]] = word[-1]
+            word = word[:-1]
+            print(word)
+            print(dict)
+            
+
+             
+        word = ''.join(word)
+        self.set_source_word(word)    
+           
+
+    def insert(self, letter):
+        word = list(self.get_src_word())
+        dict = self.get_source_dict()
+        if "#" in word:
+            word[dict['#']] = letter
+            dict[letter] = dict['#']
+            del dict['#']
+        elif "#" not in word:
+            word += letter 
+            dict[letter] = len(self.get_src_word())   
+        word = ''.join(word)
+        self.set_source_word(word)
+
+    def replace(self, original_letter, new_letter, first_delete):
+        if first_delete:
+            self.delete(original_letter,first_delete)
+            self.insert(new_letter)   
+        else:
+            self.insert(new_letter)
+            self.delete(original_letter,first_delete)    
+
+    def get_my_matrix(self):
+        return self.__my_mat
+
+if __name__ == "__main__":
+    m = MED()
+    print(m.get_src_word())
+    m.replace('l','a', False)
+    # print(m.get_src_word())
+    # print(m.get_source_dict())
+    # m.replace('p', 's')
+    # m.replace('l','t')
+    # print(m.get_src_word())
+    
